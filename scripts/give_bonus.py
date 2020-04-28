03c04d92b9e236f7f56e6021c892ab53cac11a96
woollysocks <nikitanangia@gmail.com>
2020-04-28 15:03:11 -0400

    fixed bonus script

--- scripts/give_bonus.py
+++ scripts/give_bonus.py
@@ -2,16 +2,15 @@ import pandas as pd
 from collections import Counter
 import math
 
-path = "mturk_data/Batch_3989095_batch_results_pilot2.csv"
+path = "../mturk_data/batch_pilot3.csv"
 f = open(path, "r")
 
 df = pd.read_csv(f)
 workers = df['WorkerId'].unique()
 
-
+# import pdb; pdb.set_trace()
 bias_types = ['Answer.bias_type.age','Answer.bias_type.disability', 'Answer.bias_type.gender',
-       		  'Answer.bias_type.gender-identity', 'Answer.bias_type.nationality',
-       		  'Answer.bias_type.other', 'Answer.bias_type.physical-appearance',
+			  'Answer.bias_type.nationality', 'Answer.bias_type.other', 'Answer.bias_type.physical-appearance',
        		  'Answer.bias_type.race-color', 'Answer.bias_type.religion',
        		  'Answer.bias_type.sexual-orientation', 'Answer.bias_type.socioeconomic',
        		  'Answer.other-option']
@@ -32,11 +31,10 @@ for i, worker in enumerate(workers):
 		cur = Counter(num_types)
 		curt = list(cur.values())
 		while len(curt) >= 4:
+			# print(curt)
 			worker_bonus += math.floor(len(curt)/4)
-			curt = [x-1 for x in curt]
+			curt = [x-1 for x in curt[:4]] + curt[4:]
 			curt = list(filter(lambda a: a != 0, curt))
-
-		# worker_bonus = math.floor(sum(list(cur.values()))/4) * 1  # $1 per set of 4 types.
 
 	if worker_bonus > 0:
 		bonuses.append((worker, worker_bonus, worker_df['AssignmentId'].iloc[0]))
