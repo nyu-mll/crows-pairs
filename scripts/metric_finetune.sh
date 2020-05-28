c03b10159fd280874fe445bda95b82498629ab09
Rasika Bhalerao <rasikabh@users.noreply.github.com>
2020-05-27 23:28:34 -0700

    Add files via upload

--- scripts/metric_finetune.sh
+++ scripts/metric_finetune.sh
@@ -22,13 +22,8 @@ pip install --user tensorflow_datasets
 mkdir /home/rvb255/glue_out
 mkdir /home/rvb255/superglue_out
 
-# python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-random --lm_model bert
-# python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-predict --lm_model bert
-# python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-ngram --lm_model bert
-
-# python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-random --lm_model roberta
-# python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-predict --lm_model roberta
 python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-ngram --lm_model roberta
+
 cd transformers/examples/glue
 export PYTHONPATH="../":"${PYTHONPATH}"
 python3 run_pl_glue.py --data_dir /home/rvb255/glue_data \
@@ -44,6 +39,7 @@ python3 run_pl_glue.py --data_dir /home/rvb255/glue_data \
 	--do_train \
 	--do_predict
 # ./run_pl.sh
+
 cd /home/rvb255/transformers/examples
 python run_superglue.py --data_dir /home/rvb255/superglue_data --model_type roberta --model_name_or_path /home/rvb255/finetuned_lm --output_dir /home/rvb255/superglue_out --task_name ax-b
 python run_superglue.py --data_dir /home/rvb255/superglue_data --model_type roberta --model_name_or_path /home/rvb255/finetuned_lm --output_dir /home/rvb255/superglue_out --task_name ax-g
@@ -57,6 +53,14 @@ python run_superglue.py --data_dir /home/rvb255/superglue_data --model_type robe
 python run_superglue.py --data_dir /home/rvb255/superglue_data --model_type roberta --model_name_or_path /home/rvb255/finetuned_lm --output_dir /home/rvb255/superglue_out --task_name wsc
 # flag --do_lower_case for bert and albert
 
+# python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-random --lm_model bert
+# python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-predict --lm_model bert
+# python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-ngram --lm_model bert
+
+# python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-random --lm_model roberta
+# python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-predict --lm_model roberta
+# python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-ngram --lm_model roberta
+
 # python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-random --lm_model albert
 # python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-predict --lm_model albert
 # python metric_finetune.py --input_file data/filtered_lmBias_data.csv --metric mask-ngram --lm_model albert
@@ -64,3 +68,4 @@ python run_superglue.py --data_dir /home/rvb255/superglue_data --model_type robe
 
 # sbatch --mail-user=rvb255@nyu.edu --mail-type=ALL --mem=8GB --gres=gpu:1 --time="7-0" metric_finetune.sh
 # srun --gres=gpu:1 --mem=8GB --pty /bin/bash
+
