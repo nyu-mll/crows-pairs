f196930278795813eb7f64a5c369b56f1874edc7
claravania <clara.vaniia@gmail.com>
2020-05-28 22:37:39 -0400

    example sbatch

---
+++ sbatch/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=lm-bias
+#SBATCH --output=/scratch/cv50/nlu-debiasing-data/log/finetune-%j.out
+#SBATCH --error=/scratch/cv50/nlu-debiasing-data/log/finetune-%j.out
+#SBATCH --gres=gpu:p40:1
+#SBATCH --time=48:00:00
+#SBATCH --mem=30000
+#SBATCH --signal=USR1@600
+#SBATCH --mail-user=cv50@nyu.edu
+#SBATCH --mail-type=END,FAIL
+
+
+source activate jiant
+
+python scripts/metric_finetune.py --input_dir data/cross-val --lm_model bert --max_epochs 10 --fold 0 --lr 0.001 
